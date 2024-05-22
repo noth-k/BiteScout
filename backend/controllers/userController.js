fcbafd3f313d6c481c9f6e697e78841e43425738
Vinoth K <vinoth2002k@gmail.com>
2024-05-23 00:57:02 +0800

    backend app and functions

---
+++ backend/controllers/userController.js
@@ -0,0 +1,42 @@
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken');
+
+
+//mongoDb uses _id so better to use same name --> part of the payload of the token
+const createToken = (_id) => {
+    //1st arguemnt is id,second is secret string only known to the server
+    //3rd argument to decleare how many days the person will stay logged in 
+    return jwt.sign({_id: _id}, process.env.SECRET, { expiresIn: '3d' })
+}
+
+// login user
+const loginUser = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.login(email, password);
+        //create token
+        const token = createToken(user._id)
+        res.status(200).json({email, token});
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+// signup user
+//in a route handler, can only use res.json once *** multiple calls will lead to errors
+const signupUser = async (req, res) => {
+    const { email, password } = req.body;
+    //to catch error: "email alr in use" or if input does not follow the Schema
+    try {
+        const user = await User.signup(email, password);
+        //create tokem
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
+
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+module.exports = { loginUser, signupUser };
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+//order here matters
+
+//middleware that parses incoming requests; transforms the raw request body into a JS object that can be accessed through req.body
+app.use(express.json());
+//routes
+app.use('/api/user', userRoutes);
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    //listen to request when connected
+    app.listen(process.env.PORT, () => {
+        console.log("port is listening", process.env.PORT)
+    })
+})
+.catch((error) => {
+    console.log(error);
+})
+
