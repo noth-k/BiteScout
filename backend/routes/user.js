6f3222d1d277bc41dca734341d90037b592456e6
WANG CHENGKE <ckwang@WANGs-MacBook-Pro.local>
2024-06-24 11:17:54 +0800

    added changes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const { loginUser, signupUser, hello } = require('../controllers/userController');
+const User = require('../models/userModel');
+const requireAuth = require('../middleware/auth'); // auth middleware
 
 const router = express.Router();
 
-//login route
+// get user restrictions
+router.get('/userRestrictions', requireAuth, async (req, res) => {
+  const { email } = req.query;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json({ restrictions: user.restrictions });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+// login route
 router.post('/login', loginUser);
 
-//sign up route
+// signup route
 router.post('/signup', signupUser);
 
-router.get('/hello', hello)
-
+router.get('/hello', hello);
 
 module.exports = router;
